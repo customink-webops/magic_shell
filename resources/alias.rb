54f4c30d209a7101aa3bd2f9d2177956af0b4773
Nathen Harvey <nharvey@customink.com>
2012-08-21 09:18:03 -0400

    changing the default action to :add

--- resources/alias.rb
+++ resources/alias.rb
@@ -1,6 +1,6 @@
 actions :add, :remove
 
-default_action :create
+default_action :add
 
 attribute :alias_name, :kind_of => String, :name_attribute => true
 attribute :command, :kind_of => String, :default => :add
