6471464e6ce344038bf1efda7289b9e0e6009526
Matthew Moore <matt@crowdmob.com>
2013-04-25 13:24:51 -0700

    use of default_action unless chef version is less than '0.10.10'

--- resources/environment.rb
+++ resources/environment.rb
@@ -1,6 +1,8 @@
 actions :add, :remove
 
-default_action :add
+unless Gem::Version.new(Chef::VERSION) < Gem::Version.new('0.10.10')
+  default_action :add
+end
 
 attribute :environment_variable, :kind_of => String, :name_attribute => true
 attribute :value, :kind_of => String, :default => :add
