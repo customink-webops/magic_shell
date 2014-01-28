0e49c8237c504963f1ffdea2f26834ec4c2a2332
anthroprose <anthroprose@gmail.com>
2014-01-28 12:15:11 -0600

    Added resource options for virtualenv use (or other custom permissions/locations)

--- resources/environment.rb
+++ resources/environment.rb
@@ -25,3 +25,7 @@ default_action :add
 
 attribute :environment_variable, kind_of: String, name_attribute: true
 attribute :value,                kind_of: String
+attribute :owner,                kind_of: String, default: "root"
+attribute :group,                kind_of: String, default: "root"
+attribute :mode,                 kind_of: String, default: "0755"
+attribute :destination,          kind_of: String, default: "/etc/profile.d"
